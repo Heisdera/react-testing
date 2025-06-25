709b59c066a0d73ca76edb8f3d7fb98a5b76c6d0
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-06-26 00:15:34 +0100

    feat: code coverage reporting
    - A metric that can help you understand how much of your software code is tested
    - Statement, Branch, Function, Line coverage
    - Used --watchAll flag to watch for all test in the project
    - Used collectCoverageFrom='src/components/**/*.{ts,tsx}' flag to narrow down which folder to collect coverage from
    - Used --collectCoverageFrom='!src/components/**/*.{types,stories,constants,test,spec}.{ts,tsx}' flag to exempt what files to not collect coverage from
    - Setup coverage threshold in package.json making sure jest fails if it doesn't meet the threshold requirements

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -10,21 +10,10 @@ describe("Greet", () => {
   test("Greet renders Hello successfully", () => {
     render(<Greet />);
     // const textElement = screen.getByText(/HELLO/i); // this is a regex pattern to ignore case sensitivity
-    const textElement = screen.getByText("Hello");
+    const textElement = screen.getByText(/Hello/);
 
     // check if the text "Hello" is present
     expect(textElement).toBeInTheDocument();
-  });
-
-  describe("Nested Test", () => {
-    test("Greet renders Hello Dera successfully", () => {
-      render(<Greet name="Dera" />);
-
-      const textElement = screen.getByText("Hello Dera");
-
-      // check if the text "Hello Dera" is present
-      expect(textElement).toBeInTheDocument();
-    });
   });
 });
 
