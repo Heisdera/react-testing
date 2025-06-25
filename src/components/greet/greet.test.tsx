197e3780f32b816bd2ca014597db3ffe545391fb
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-06-25 23:28:22 +0100

    feat: grouping tests using describe blocks

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -6,40 +6,26 @@ import Greet from "./greet";
  * If a name is passed into the component, it should render 'hello', followed by the name.
  */
 
-test("Greet renders successfully", () => {
-  render(<Greet />);
-  // const textElement = screen.getByText(/HELLO/i); // this is a regex pattern to ignore case sensitivity
-  const textElement = screen.getByText("Hello");
+describe("Greet", () => {
+  test("Greet renders Hello successfully", () => {
+    render(<Greet />);
+    // const textElement = screen.getByText(/HELLO/i); // this is a regex pattern to ignore case sensitivity
+    const textElement = screen.getByText("Hello");
 
-  // check if the text "Hello" is present
-  expect(textElement).toBeInTheDocument();
+    // check if the text "Hello" is present
+    expect(textElement).toBeInTheDocument();
+  });
+
+  describe("Nested Test", () => {
+    test("Greet renders Hello Dera successfully", () => {
+      render(<Greet name="Dera" />);
+
+      const textElement = screen.getByText("Hello Dera");
+
+      // check if the text "Hello Dera" is present
+      expect(textElement).toBeInTheDocument();
+    });
+  });
 });
 
-// test("Greet renders with a name", () => {
-//   render(<Greet name="Dera" />);
-
-//   const textElement = screen.getByText("Hello Dera");
-
-//   // check if the text "Hello Dera" is present
-//   expect(textElement).toBeInTheDocument();
-// });
-
-// "test.only" makes sure only that test runs on save
-test.only("Greet renders with a name", () => {
-  render(<Greet name="Dera" />);
-
-  const textElement = screen.getByText("Hello Dera");
-
-  // check if the text "Hello Dera" is present
-  expect(textElement).toBeInTheDocument();
-});
-
-// "test.skip" makes sure only that test is skipped on save
-test.skip("Greet renders Hello Dera", () => {
-  render(<Greet name="Dera" />);
-
-  const textElement = screen.getByText("Hello Dera");
-
-  // check if the text "Hello Dera" is present
-  expect(textElement).toBeInTheDocument();
-});
+// Note: --- One file === One test Suite --- (test suites are not determined by describe block) && .only & .skip also applies to describe blocks not just test
