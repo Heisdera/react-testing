9dc3175bc9b2f5ce0fbe5aa5c67f9edcd2eb690a
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-06-25 23:03:25 +0100

    feat: add greet component and greet test

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./greet";
+
+/**
+ * Greet should render the text 'hello'.
+ * If a name is passed into the component, it should render 'hello', followed by the name.
+ */
+
+test("Greet renders successfully", () => {
+  render(<Greet />);
+  // const textElement = screen.getByText(/HELLO/i); // this is a regex pattern to ignore case sensitivity
+  const textElement = screen.getByText("Hello");
+
+  // check if the text "Hello" is present
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Dera" />);
+
+  const textElement = screen.getByText("Hello Dera");
+
+  // check if the text "Hello Dera" is present
+  expect(textElement).toBeInTheDocument();
+});
