617b559c46a435dbe631915ebf4e1694173e9743
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-06-26 12:26:13 +0100

    feat: RTL Queries  -  getByAltText

--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -4,6 +4,10 @@ export const Application = () => {
       <h1>Job Application Form</h1>
       <h2>Section 1</h2>
       <p>All fields are mandatory</p>
+      <img
+        src="https://images.unsplash.com/photo-1726160423882-7c6cfc1687a4?q=80&w=474&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="a person with a laptop coding"
+      />
       <form>
         <div>
           <label htmlFor="name">Name</label>
