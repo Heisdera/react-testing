eb92c9c08cf0872eccacfacc1ee2b752ddf1c24b
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-06-28 23:59:37 +0100

    feat: testing keyboard interactions

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <input
+        type="number"
+        name="amount"
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+        placeholder="Enter amount"
+        aria-label="Amount to set counter"
+      />
+      <button type="button" onClick={() => setCount(amount)}>
+        Set
+      </button>
     </div>
   );
 };
